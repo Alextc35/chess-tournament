725b7246c6b86436173f2f07a048cbda9bc54d08
Alejandro Téllez <119206974+Alextc35@users.noreply.github.com>
2024-11-16 15:41:04 +0100

    creada plantilla que será la lista de participantes, footer hecho con año y licencia (version 0.0.1)

--- src/index.php
+++ src/index.php
@@ -9,17 +9,42 @@
 <body>
 
     <div class="aplicacion">
+
         <header class="header">
             <h1 class="header-title">Torneo de Ajedrez</h1>
-            <p class="header-author">Aplicación creada por @alextc35</p>
+            <p class="header-author">Aplicación creada por
+                <a href="https://www.linkedin.com/in/alejandrotellezcorona/" class="linkedin" target="_blank">
+                    @alextc35
+                </a>
+            </p>
         </header>
-        <div class="add-players">
-            <form action="" method="POST" class="form">
+
+        <section class="add-players">
+            <form action="" method="POST" class="form" id="players-form">
                 <label for="addPlayer" class="form-label">Jugadores:</label>
                 <input type="text" name="addPlayer" id="addPlayer" placeholder="Introduce el nombre de un participante del torneo..."/>
                 <button type="submit" id="button-addPlayer">Añadir</button>
             </form>
-        </div>
+        </section>
+
+        <section class="view-players">
+            <form action="" method="post" class="form-players">
+                <h1 class="h1-view">Participantes</h1>
+                <div class="players">
+                    <input type="text" class="view-name" value="Alejandro Téllez Corona" disabled/>
+                    <input type="text" class="view-name" value="Daniel Ramos Estebán" disabled/>
+                </div>
+                <button type="submit" id="button-matchPlayer">Enfrentar</button>
+            </form>
+        </section>
+
+        <footer class="footer">
+            <p>&copy; <?=date('Y')?> | 
+                <a href="https://github.com/Alextc35/chess-tournament/blob/main/LICENSE" class="license" target="_blank"> Licencia MIT</a>
+            </p>
+            <p>v. 0.0.1</p>
+        </footer>
+
     </div>
 </body>
 </html>
