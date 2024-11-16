8c094af38e18d4eaf5b7d2ca2a8569a8077c3d3d
Alejandro Téllez <119206974+Alextc35@users.noreply.github.com>
2024-11-16 12:48:15 +0100

    adición de título y prompt para añadir jugadores estilado

--- src/index.php
+++ src/index.php
@@ -3,13 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Torneos de Ajedrez</title>
+    <title>Torneo de Ajedrez</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Hola Mundo!</h1>
-    <?php
-        echo "PHP version: " . PHP_VERSION;
-    ?>
+
+    <div class="aplicacion">
+        <header class="header">
+            <h1 class="header-title">Torneo de Ajedrez</h1>
+            <p class="header-author">Aplicación creada por @alextc35</p>
+        </header>
+        <div class="add-players">
+            <form action="" method="POST" class="form">
+                <label for="addPlayer" class="form-label">Jugadores:</label>
+                <input type="text" name="addPlayer" id="addPlayer" placeholder="Introduce el nombre de un participante del torneo..."/>
+                <button type="submit" id="button-addPlayer">Añadir</button>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
