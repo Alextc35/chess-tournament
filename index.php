2868f96a88d46f4486c312e2d313df9817ff562a
Alejandro Téllez <119206974+Alextc35@users.noreply.github.com>
2024-11-30 20:11:21 +0100

    optimizacion de imagenes, mejora claridad de código y adición de favicon

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ $version = "0.5.1";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Descubre nuestra aplicación web para generar emparejamientos de manera rápida y sencilla. Ideal para torneos, sorteos, actividades grupales y más. ¡Organiza y conecta a las personas fácilmente!">
     <title>Forjador de partidas</title>
+    <link rel="icon" href="favicon.png" type="image/png">
     <link rel="stylesheet" href="style.css">
     <script src="script.js"></script>
 </head>
@@ -36,7 +37,7 @@ $version = "0.5.1";
                     <ul class="no-margin-no-padding">
                         <?php foreach ($_SESSION['players'] as $index => $player): ?>
                             <li class="list-player"><?= ($index + 1) . ". " . htmlspecialchars($player) ?>
-                                <button id="editButton-<?= $index ?>" class="edit-player" onclick="showEditForm(<?= $index ?>)"><img src="./img/editar.png" class="list-player-img" alt="Editar"></button>
+                                <button id="editButton-<?= $index ?>" class="edit-player" onclick="showEditForm(<?= $index ?>)"><img src="./img/editar.webp" class="list-player-img" alt="Editar"></button>
                                 <form id="editForm-<?= $index ?>" class="edit-player-form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" style="display: none;">
                                     <input type="hidden" name="playerIndex" value="<?= $index ?>"> <!-- Input auxiliar -->
                                     <label for="editPlayer-<?= $index ?>">Renombrar:</label>
@@ -49,7 +50,7 @@ $version = "0.5.1";
 
                                 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" style="display: inline;">
                                     <input type="hidden" name="playerIndex" value="<?= $index ?>">
-                                    <button id="deleteButton-<?= $index ?>" type="submit" name="deletePlayer" class="delete-player" onclick="return confirm('¿Estás seguro de que quieres eliminar a \'<?php echo htmlspecialchars($player) ?>\'?')"><img src="./img/eliminar.png" class="list-player-img" alt="Eliminar"></button>
+                                    <button id="deleteButton-<?= $index ?>" type="submit" name="deletePlayer" class="delete-player" onclick="return confirm('¿Estás seguro de que quieres eliminar a \'<?php echo htmlspecialchars($player) ?>\'?')"><img src="./img/eliminar.webp" class="list-player-img" alt="Eliminar"></button>
                                 </form>
                             </li>
                         <?php endforeach; ?>
@@ -72,7 +73,7 @@ $version = "0.5.1";
     </main>
 
     <footer class="column-content">
-        <p>&copy; <?=date('Y')?> |<a href="https://github.com/Alextc35/match-forge/blob/main/LICENSE" class="footer-license" target="_blank">Licencia MIT</a></p>
+        <p>&copy; <?=date('Y')?> | <a href="https://github.com/Alextc35/match-forge/blob/main/LICENSE" class="footer-license" target="_blank">Licencia MIT</a></p>
         <p><a href="https://github.com/Alextc35/match-forge" class="footer-version" target="_blank">v. <?= $version ?></a></p>
     </footer>
 </body>
